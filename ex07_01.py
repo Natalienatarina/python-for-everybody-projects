986742444ed3b566c2489c819940be44315e5674
Natalienatarina <natthamonbandasak@gmail.com>
2025-09-30 13:21:24 +0200

    Delete ex07_01.py

--- ex07_01.py
+++
@@ -1,9 +0,0 @@
-fname = input('Enter file name: ')
-try: 
-    fhand = open(fname)
-except FileNotFoundError:
-    print(f'File cannot be opened: {fname}')
-    quit()
-for line in fhand:
-    line = line.rstrip().upper()
-    print(line)
